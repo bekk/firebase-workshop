a618ed1e3c77217e3643fa21c7fc51bda9e602e8
carljoachim <carljoachim@bekk-mac-2495.home>
2022-01-06 13:30:22 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React  from 'react';
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 
 const App = () => {
     return (
-        <div className="App">    
+        <div>    
             <Routes>
                 <Route path="/" element={ <Home /> } />
             </Routes>
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {  
     return (
-        <div className="App">
+        <div>
             <h1>Dinner Planner</h1>
         </div>
     );
