71d3764ee151b78138d8af289d9f4598df34846f
carljoachim <carljoachim@bekk-mac-2495.home>
2022-01-06 12:42:08 +0100

    Init commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {  
+    return (
+        <div className="App">
+            <h1>Dinner Planner</h1>
+        </div>
+    );
+}
+
+export default Home;
