57f833a588a0d8aa3a8da3c7872c4f0a58065730
Ole Reidar Holm <ole.reidar.holm@entur.org>
2022-03-28 10:44:07 +0200

    Fix wrong import path

--- src/Containers/Questions.js
+++ src/Containers/Questions.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { getQuestions } from './firebase'
+import { getQuestions } from '../firebase'
 
 const Questions = () => {  
     const navigate = useNavigate();
